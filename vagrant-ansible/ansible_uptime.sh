4772a709909c322c0135eeab550d5b88d19aee97
Gianluca Genga <gianluca.genga@mondora.com>
2015-11-11 18:26:02 +0100

    modificata demo per single environment

--- vagrant-ansible/ansible_uptime.sh
+++ vagrant-ansible/ansible_uptime.sh
@@ -1,1 +1,1 @@
-ansible ansibleHost -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory -a "uptime" -u vagrant --private-key=~/mondora/corsoAnsible/ansible-demo/vagrant/.vagrant/machines/default/virtualbox/private_key
+ansible all -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory -a "uptime" -u vagrant --private-key=~/mondora/corsoAnsible/ansible-demo/vagrant/.vagrant/machines/default/virtualbox/private_key
