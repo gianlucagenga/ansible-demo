9a284d8d335c0f0ef98b8cf9abb8f78649979e2f
Gianluca Genga <gianluca.genga@mondora.com>
2015-11-22 12:19:27 +0100

    demo1-bis

--- 1-ansiblecommands/ansible_uptime.sh
+++ 1-ansiblecommands/ansible_uptime.sh
@@ -1,3 +1,1 @@
-ssh-add ~/mondora/corsoAnsible/ansible-demo/vagrant/.vagrant/machines/default/virtualbox/private_key
-
 ansible all -i hosts -a "uptime"
--- 1-ansiblecommands/ansible_yum_nginx.sh
+++ 1-ansiblecommands/ansible_yum_nginx.sh
@@ -1,3 +1,1 @@
-ssh-add ~/mondora/corsoAnsible/ansible-demo/vagrant/.vagrant/machines/default/virtualbox/private_key
-
 ansible all -i hosts -s -m apt -a 'pkg=nginx state=installed update_cache=true'
