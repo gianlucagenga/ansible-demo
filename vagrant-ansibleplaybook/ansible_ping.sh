aa7f625e337b01bddc5809edccc9fc2ff03fcca1
Gianluca Genga <gianluca.genga@mondora.com>
2015-11-22 11:18:26 +0100

    revisione esercitazione

--- vagrant-ansibleplaybook/ansible_ping.sh
+++ vagrant-ansibleplaybook/ansible_ping.sh
@@ -1,1 +1,3 @@
 ansible all -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory -m ping -u vagrant --private-key=~/mondora/corsoAnsible/ansible-demo/vagrant/.vagrant/machines/default/virtualbox/private_key
+
+ansible webservers -m ping
