05b7e5c3af635d5f5596916944708e7da833a0e9
Gianluca Genga <gianluca.genga@mondora.com>
2016-05-22 17:21:25 +0200

    modifica caso windows

--- vagrant-ansiblewindows/bootstrap.sh
+++ vagrant-ansiblewindows/bootstrap.sh
@@ -1,3 +1,5 @@
 sudo -E apt-get update
 sudo -E apt-get install -y unzip python-pip python-virtualenv python-dev
 sudo -E pip install ansible
+cd /home/vagrant/src
+ansible-playbook -i hosts site.yml
